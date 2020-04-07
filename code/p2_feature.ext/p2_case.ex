d92027827152440a4544e4023db97f4b18276b94
JohnsonBen00 <42557348+JohnsonBen00@users.noreply.github.com>
2020-04-06 18:47:37 -0700

    Update p2_case.ex

--- code/p2_feature.ext/p2_case.ex
+++ code/p2_feature.ext/p2_case.ex
@@ -5,8 +5,8 @@ defmodule Case do
 		case {"Hi", "Hello"} do
 			{"Hi", x} -> IO.puts("The computer response woud be to say 'Hello'")
   			{"What", "Excuse Me"} -> IO.puts("That was not a greeting")
-			end
+		end
 
-  		end
+  	end
 
 end
