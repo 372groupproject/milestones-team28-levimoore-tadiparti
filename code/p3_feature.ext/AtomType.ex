f412aebfb4b69630de9ac4a67ac25b235267737d
lmoore22 <59933337+lmoore22@users.noreply.github.com>
2020-04-10 20:42:45 -0700

    Update AtomType.ex

--- code/p3_feature.ext/AtomType.ex
+++ code/p3_feature.ext/AtomType.ex
@@ -1,24 +1,24 @@
 defmodule AtomType do
 
-	def newAtom() do
-		atom = :apple
-		IO.puts atom
-	end 
+    def newAtom() do
+        atom = :apple
+        IO.puts atom
+    end
 
-	def isEqual() do
-		:apple == :apple
-		:orange == :orange
-	end
+    def isEqual() do
+        IO.puts :apple == :apple
+        :orange == :orange
+    end
 
-	def isBoolAtom() do
-		:true == true
-		is_atom(false)
-		is_boolean(:false)
-	end
+    def isBoolAtom() do
+        IO.puts :true == true
+        IO.puts is_atom(false)
+        is_boolean(:false)
+    end
 
-	def otherFunct(atom) do
-		Atom.to_charlist(atom)
-		Atom.to_string(atom)
-	end
+    def otherFunct(atom) do
+        IO.puts Atom.to_charlist(atom)
+        Atom.to_string(atom)
+    end
 
 end
