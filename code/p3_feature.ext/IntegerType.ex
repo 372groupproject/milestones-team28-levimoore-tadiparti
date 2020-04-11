ce79c121a0eee0f3af6739ee22cd85771441be78
lmoore22 <59933337+lmoore22@users.noreply.github.com>
2020-04-10 21:00:23 -0700

    Update IntegerType.ex

--- code/p3_feature.ext/IntegerType.ex
+++ code/p3_feature.ext/IntegerType.ex
@@ -1,23 +1,19 @@
+
 defmodule IntegerType do
 
-	def arithmetic(x, y) do
-		IO.puts x + y
-		IO.puts x * y
-		IO.puts x - y
-		IO.puts x / y
-	end
-	
-	def otherfunct(x, y) do
-		Integer.digits(x)
-		Integer.floor_div(x, y)
-		Integer.gcd(x, y)
-		Integer.is_even(x)
-		Integer.is_odd(x)
-		Integer.mode(x, y)
-		Integer.parse(x)
-		Integer.to_charlist(x)
-		Integer.to_string(x)
-		Integer.undigits(x)
-	end
+    def arithmetic(x, y) do
+        IO.puts x + y
+        IO.puts x * y
+        IO.puts x - y
+        IO.puts x / y
+    end
+
+    def otherFunct(x, y) do
+        IO.puts Integer.floor_div(x, y)
+        IO.puts Integer.gcd(x, y)
+        IO.puts Integer.mod(x, y)
+        IO.puts Integer.to_charlist(x)
+        IO.puts Integer.to_string(x)
+    end
 
 end
