046156da82691203c543c1792c6e796aeecd7c28
lmoore22 <59933337+lmoore22@users.noreply.github.com>
2020-04-11 22:08:44 -0700

    Update GradeCalc.ex

--- code/p3_feature.ext/GradeCalc.ex
+++ code/p3_feature.ext/GradeCalc.ex
@@ -1,39 +1,39 @@
+
 defmodule GRADE do
 
-	def calc(name, class, assignments, midterms, final, project, participation) do
+    def calc(name, class, assignments, midterms, final, project, participation) do
 
-		sumAssignments = 0.0
-		for m <- assignments, do: sumAssignments = sumAssignments + m
+        sumAssignments = Enum.reduce assignments, 0, fn(x, total) ->
+            x + total
+        end
 
-		sumMidterms = 0.0
-		for n <- midterms, do: sumMidterms = sumMidterms + n
-		
-		overAll = sumAssignments + sumMidterms + final + project + participation
+        sumMidterms = Enum.reduce midterms, 0, fn(x, total) ->
+            x + total
+        end
 
-		grade = overAll / 100.0
-		
-		if grade >= 90.0 do
-			letter = :A
-		unless grade >= 80.0 do
-			letter = :B
-		unless grade >= 70.0 do
-			letter = :C
-		unless grade >= 60.0 do
-			letter = :D
-		else
-			letter = :E
-		end
+        grade = ((sumAssignments / 8) * 0.24) + ((sumMidterms /2 ) * 0.30) + (final * 0.30) + (project * 0.10) + (participation * 0.06)
 
-		if grade >= 60.0 do
-			message = <> name <> " passed " <> class <> "!"
-  			IO.puts message
-		else
-			message = <> name <> " failed " <> class <> "!"
-  			IO.puts message
-		end
+        if grade >= 60.0 do
+            message = name <> " passed " <> class <> "!"
+            IO.puts message
 
-		IO.puts "Grade: " + letter + "{" + grade + "}"
+        else
+            message = name <> " failed " <> class <> "!"
+            IO.puts message
+        end
 
-	end
+        grade = Float.to_string(grade)
 
+        cond do
+            grade >= 90.0 -> IO.puts "Grade: " <> "#{:A}" <> " {" <> grade <> "} "
+
+            grade >= 80.0 -> IO.puts "Grade: " <> "#{:B}" <> " {" <> grade <> "} "
+
+            grade >= 70.0 -> IO.puts "Grade: " <> "#{:C}" <> " {" <> grade <> "} "
+
+            grade >= 60.0 -> IO.puts "Grade: " <> "#{:D}" <> " {" <> grade <> "} "
+
+            true ->  IO.puts "Grade: " <> "#{:E}" <> " {" <> grade <> "} "
+        end
+    end
 end
